6d4d7da689307cb2d4921c3906b61dce8fa3f88e
zgiarida <zgiarida@gmail.com>
2022-12-31 17:39:12 +0700

    install react router dan dom, create component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Offers from "./pages/Offers"
+import ForgotPassword from "./pages/ForgotPassword"
 function App() {
+
   return (
-    <div className="App">
-        <p className='text-3xl bg-slate-500 border-black'>Ini Adalah Latihan React</p>
-    </div>
-  );
+
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/offers" element={<Offers/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+      </Routes>
+    </Router>
+    </>
+  )
 }
 
 export default App;
